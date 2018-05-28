85cca5727b2faa3915c3f1d87475073e44d8f9ec
marat <pcgeniusweb.mm@gmail.com>
2018-05-28 16:50:29 +0300

    Сделал jd01_05 TaskB2

---
+++ project/src/net/mustaphin/project/action/area/AbstractArea.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mustaphin.project.action.area;
+
+/**
+ *
+ * @author marat
+ */
+public class AbstractArea {
+    
+}
---
+++ project/src/net/mustaphin/project/action/area/AbstractAreaFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.mustaphin.project.action.area;
+
+/**
+ *
+ * @author marat
+ */
+public abstract class AbstractAreaFactory {
+
+    public abstract double find();
+}
