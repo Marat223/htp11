fb3b18e1b2683442313659bcd863c6ad41f064d6
marat <pcgeniusweb.mm@gmail.com>
2018-06-14 13:51:06 +0300

    Сделал jd01_05 TaskB2

--- project/src/net/mustaphin/project/repository/SpecificationByPerimeterRange.java
+++ project/src/net/mustaphin/project/repository/SpecificationByPerimeterRange.java
@@ -5,39 +5,14 @@
  */
 package net.mustaphin.project.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import net.mustaphin.project.parameter.GeometricalParameter;
-import net.mustaphin.project.parameter.registrator.Registrator;
-import net.mustaphin.project.shape.Tetragon;
-
 /**
  *
  * @author marat
  */
-public class SpecificationByPerimeterRange implements ISpecification {
-
-    private double min;
-    private double max;
+public class SpecificationByPerimeterRange extends SpecificationByAreaRange {
 
     public SpecificationByPerimeterRange(double min, double max) {
-	this.min = min;
-	this.max = max;
-    }
-
-    @Override
-    public List<Tetragon> specified(List<Tetragon> repository) {
-	List<Tetragon> tetragons = new ArrayList<>();
-	Registrator registrator = Registrator.getInstance();
-	List<GeometricalParameter> parameters = registrator.getParameters();
-	for (GeometricalParameter parameter : parameters) {
-	    double perimeter = parameter.getPerimeter();
-	    if (min < perimeter && max > perimeter) {
-		int id = registrator.getId(parameter);
-		tetragons.add(repository.get(id));
-	    }
-	}
-	return tetragons;
+	super(min, max);
     }
 
 }
